48fc0b09a3703d02ff3126e6574f8d657f59e9cd
faseeh <m.faseeh1990@gmail.com>
2021-05-13 05:55:05 -0700

    FlutterApp

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Faseeh Ahmed Id: m.faseeh1990@gmail.com"),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+  runApp(MyApp());
+}
